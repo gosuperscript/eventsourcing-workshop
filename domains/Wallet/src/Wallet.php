5c326277bb151577b03a698e12036af7531c0d9a
Robertbaelde <robert_baelde@hotmail.com>
2022-10-04 17:05:14 +0200

    remove some more

--- domains/Wallet/src/Wallet.php
+++ domains/Wallet/src/Wallet.php
@@ -4,61 +4,8 @@ namespace Workshop\Domains\Wallet;
 
 use EventSauce\EventSourcing\AggregateRoot;
 use EventSauce\EventSourcing\AggregateRootBehaviour;
-use Workshop\Domains\Wallet\Events\TokensDeposited;
-use Workshop\Domains\Wallet\Events\TokensWithdrawn;
-use Workshop\Domains\Wallet\Events\TransferInitiated;
-use Workshop\Domains\Wallet\Events\WithdrawalFailed;
-use Workshop\Domains\Wallet\Exceptions\SorryCantWithdraw;
-use Workshop\Domains\Wallet\Transactions\TransactionId;
 
 class Wallet implements AggregateRoot
 {
     use AggregateRootBehaviour;
-
-    private int $tokens = 0;
-
-    public function deposit(int $tokens, string $description, \DateTimeImmutable $transactedAt, ?TransactionId $transactionId = null): void
-    {
-        $this->recordThat(new TokensDeposited($tokens, $description, $transactedAt, $transactionId));
-    }
-
-    public function withdraw(int $tokens, string $description, \DateTimeImmutable $transactedAt, ?TransactionId $transactionId = null)
-    {
-        if($this->tokens < $tokens) {
-            $this->recordThat(WithdrawalFailed::becauseOfInsufficientFunds($transactionId));
-            throw SorryCantWithdraw::becauseOfInsufficientFunds();
-        }
-        $this->recordThat(new TokensWithdrawn($tokens, $description, $transactedAt, $transactionId));
-    }
-
-    public function transfer(
-        Transactions\TransactionId $transactionId,
-        WalletId $receivingWalletId,
-        int $tokens,
-        string $description,
-        \DateTimeImmutable $now
-    ) {
-        if($this->tokens < $tokens){
-            return;
-        }
-        $this->recordThat(new TransferInitiated($transactionId, $receivingWalletId, $tokens, $description, $now));
-    }
-
-    private function applyTokensDeposited(TokensDeposited $event): void
-    {
-        $this->tokens += $event->tokens;
-    }
-
-    private function applyTokensWithdrawn(TokensWithdrawn $event): void
-    {
-        $this->tokens -= $event->tokens;
-    }
-
-    private function applyWithdrawalFailed(WithdrawalFailed $event): void
-    {
-    }
-
-    private function applyTransferInitiated(TransferInitiated $event): void
-    {
-    }
 }
--- domains/Wallet/src/WalletServiceProvider.php
+++ domains/Wallet/src/WalletServiceProvider.php
@@ -2,92 +2,40 @@
 
 namespace Workshop\Domains\Wallet;
 
-use EventSauce\Clock\Clock;
-use EventSauce\Clock\SystemClock;
 use EventSauce\EventSourcing\DefaultHeadersDecorator;
-use EventSauce\EventSourcing\ExplicitlyMappedClassNameInflector;
-use EventSauce\EventSourcing\MessageDecoratorChain;
+use EventSauce\EventSourcing\DotSeparatedSnakeCaseInflector;
 use EventSauce\EventSourcing\MessageDispatcherChain;
 use EventSauce\EventSourcing\Serialization\ConstructingMessageSerializer;
-use EventSauce\EventSourcing\Serialization\ObjectMapperPayloadSerializer;
-use EventSauce\EventSourcing\SynchronousMessageDispatcher;
-use EventSauce\EventSourcing\Upcasting\UpcasterChain;
-use EventSauce\EventSourcing\Upcasting\UpcastingMessageSerializer;
 use EventSauce\MessageRepository\TableSchema\DefaultTableSchema;
 use EventSauce\UuidEncoding\BinaryUuidEncoder;
 use Illuminate\Database\DatabaseManager;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
-use League\Tactician\Handler\Locator\InMemoryLocator;
-use Workshop\Domains\Wallet\Commands\DepositTokens;
-use Workshop\Domains\Wallet\Commands\WithdrawTokens;
-use Workshop\Domains\Wallet\Decorators\EventIDDecorator;
-use Workshop\Domains\Wallet\Decorators\ProcessIdsDecorator;
-use Workshop\Domains\Wallet\Decorators\RandomNumberHeaderDecorator;
-use Workshop\Domains\Wallet\Infra\EloquentTransactionsReadModelRepository;
-use Workshop\Domains\Wallet\Infra\IlluminateWalletBalanceRepository;
-use Workshop\Domains\Wallet\Infra\TransactionsReadModelRepository;
-use Workshop\Domains\Wallet\Infra\WalletBalanceRepository;
 use Workshop\Domains\Wallet\Infra\WalletMessageRepository;
 use Workshop\Domains\Wallet\Infra\WalletRepository;
-use Workshop\Domains\Wallet\Projectors\TransactionsProjector;
-use Workshop\Domains\Wallet\Projectors\WalletBalanceProjector;
-use Workshop\Domains\Wallet\Upcasters\BalanceUpcaster;
-use Workshop\Domains\Wallet\Upcasters\TransactedAtUpcaster;
 
 class WalletServiceProvider extends ServiceProvider
 {
 
     public function register()
     {
-
-        $this->app->bind(TransactionsReadModelRepository::class, fn(Application $app) => new EloquentTransactionsReadModelRepository());
-        $this->app->bind(WalletBalanceRepository::class, fn(Application $app) => new IlluminateWalletBalanceRepository());
-
-        $this->app->bind(Clock::class, fn(Application $app) => new SystemClock());
-
-        // This should live in a config file.
-        $classNameInflector = new ExplicitlyMappedClassNameInflector(config('eventsourcing.class_map'));
-
-        $this->app->bind(WalletMessageRepository::class, function (Application $application) use ($classNameInflector) {
+        $this->app->bind(WalletMessageRepository::class, function (Application $application) {
             return new WalletMessageRepository(
                 connection: $application->make(DatabaseManager::class)->connection(),
                 tableName: 'wallet_messages',
-                serializer: new UpcastingMessageSerializer(
-                    eventSerializer: new ConstructingMessageSerializer(
-                        classNameInflector: $classNameInflector
-                    ),
-                    upcaster: new UpcasterChain(
-                         new TransactedAtUpcaster()
-                    )
-                ),
+                serializer: new ConstructingMessageSerializer(),
                 tableSchema: new DefaultTableSchema(),
                 uuidEncoder: new BinaryUuidEncoder(),
             );
         });
 
-        $this->app->bind(WalletRepository::class, function () use ($classNameInflector) {
+        $this->app->bind(WalletRepository::class, function () {
             return new WalletRepository(
-                messageRepository: $this->app->make(WalletMessageRepository::class),
-                dispatcher: new MessageDispatcherChain(
-                    new SynchronousMessageDispatcher(
-                        $this->app->make(TransactionsProjector::class),
-                        $this->app->make(WalletBalanceProjector::class),
-                    )
-                ),
-                decorator: new MessageDecoratorChain(
-                    new EventIDDecorator(),
-                    new DefaultHeadersDecorator(inflector: $classNameInflector),
-                    new RandomNumberHeaderDecorator(),
-                    new ProcessIdsDecorator(),
-                ),
-                classNameInflector: $classNameInflector,
+                $this->app->make(WalletMessageRepository::class),
+                new MessageDispatcherChain(),
+                new DefaultHeadersDecorator(),
+                new DotSeparatedSnakeCaseInflector(),
             );
         });
-
-        /** @var InMemoryLocator $locator */
-        $locator = $this->app->make(InMemoryLocator::class);
-        $locator->addHandler($this->app->make(WalletCommandHandler::class), WithdrawTokens::class);
-        $locator->addHandler($this->app->make(WalletCommandHandler::class), DepositTokens::class);
     }
 }
